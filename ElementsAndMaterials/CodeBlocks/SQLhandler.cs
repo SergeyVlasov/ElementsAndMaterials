5da9e4caa8109f339aac4b04d7efda0b74a76dba
Sergey Mashencev <mashentsevsergeyda@outlook.com>
2022-11-19 19:14:45 +0300

    вроде как все работает

--- ElementsAndMaterials/CodeBlocks/SQLhandler.cs
+++ ElementsAndMaterials/CodeBlocks/SQLhandler.cs
@@ -9,29 +9,35 @@ namespace ElementsAndMaterials.CodeBlocks
 {
     class SQLhandler
     {
-        public static void makeRequestShowOneElement(SqlCommand com, SqlConnection con, string typeElement, string material, TextBox textBox, string nameElement)
-        {
-            makeRequestShow(com, con, typeElement, material, textBox, "select_1_element_witch_no_in_materials", nameElement);
-            //string ComText = string.Format(File.ReadAllText(FilePathFinder.GetPathToFile($"\\SQLRequests\\select_1_element_witch_no_in_materials.sql")), typeElement, nameElement);
-            //com.CommandText = ComText;
-            //textBox.Text = ComText;
-        }
         public static void makeRequestShowAllElements(SqlCommand com, SqlConnection con, string typeElement, string material, TextBox textBox)
         {
-            makeRequestShow(com, con, typeElement, material, textBox, "select_all_elements_witch_no_in_materials", "");
-        }
-        public static void makeRequesInsertOneElement(SqlCommand com, SqlConnection con, string typeElement, string material, string goodGroup, string prefix, TextBox textBox, string nameElement)
-        {
-            //makeRequesInsert(com, con, typeElement, material, goodGroup, prefix, textBox, "insert_1_element_in_materials.sql", nameElement);
-            string ComText = string.Format(File.ReadAllText(FilePathFinder.GetPathToFile($"\\SQLRequests\\insert_1_element_in_materials.sql")), typeElement, material, goodGroup, prefix, nameElement);
+            string ComText = string.Format(File.ReadAllText(FilePathFinder.GetPathToFile($"\\SQLRequests\\select_all_elements_witch_no_in_materials.sql")), typeElement);
             com.CommandText = ComText;
-            textBox.Text = ComText;
+            con.Open();
+            string result;
+            if (com.ExecuteScalar() != null)
+            {
+                textBox.Text += $"Материал: {material}, недостающие элементы: {Environment.NewLine} {Environment.NewLine}";
+                var reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    string marking = reader["marking_glasselement"].ToString();
+                    string name = reader["name_glasselement"].ToString();
+                    string id = reader["id_glasselement"].ToString();
+                    textBox.Text += $"{id}             {marking}                 {name} {Environment.NewLine}";
+                }
+            }
+            else
+            {
+                textBox.Text += $"нет элементов для вставки {Environment.NewLine}";
+                con.Close();
+            }
+            con.Close();
         }
 
-
-        private static void makeRequestShow(SqlCommand com, SqlConnection con, string typeElement, string material, TextBox textBox, string sqlFile, string nameElement)
+        public static void makeRequestShowOneElement(SqlCommand com, SqlConnection con, string typeElement, string material, TextBox textBox, string nameElement)
         {
-            string ComText = string.Format(File.ReadAllText(FilePathFinder.GetPathToFile($"\\SQLRequests\\{sqlFile}.sql")), typeElement, nameElement);
+            string ComText = string.Format(File.ReadAllText(FilePathFinder.GetPathToFile($"\\SQLRequests\\select_1_element_witch_no_in_materials.sql")), nameElement);
             com.CommandText = ComText;
             con.Open();
             string result;
@@ -88,6 +94,43 @@ namespace ElementsAndMaterials.CodeBlocks
                 con.Close();
             };
             
+            textBox.Text += $"элементы занесены в материалы {Environment.NewLine}";
+        }
+
+        public static void makeRequesInsertOneElement(SqlCommand com, SqlConnection con, string typeElement, string material, string goodGroup, string prefix, TextBox textBox, string nameElement)
+        {
+            string ComText = string.Format(File.ReadAllText(FilePathFinder.GetPathToFile($"\\SQLRequests\\select_1_element_witch_no_in_materials.sql")), nameElement);
+            com.CommandText = ComText;
+            textBox.Text = ComText;
+            con.Open();
+            string result;
+            List<string> idList = new List<string>();
+            if (com.ExecuteScalar() != null)
+            {
+                var reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    idList.Add(reader["id_glasselement"].ToString());
+                }
+            }
+            else
+            {
+                textBox.Text += $"нет элементов для вставки {Environment.NewLine}";
+                con.Close();
+                return;
+            }
+            con.Close();
+
+            foreach (string id in idList)
+            {
+                con.Open();
+                com.CommandText = string.Format(File.ReadAllText(FilePathFinder.GetPathToFile($"\\SQLRequests\\insert_all_elements_in_materials.sql")), typeElement, goodGroup, prefix, id);
+                string xxx = com.CommandText;
+                textBox.Text += xxx;
+                com.ExecuteScalar();
+                con.Close();
+            };
+
             textBox.Text += $"элементы занесены в материалы {Environment.NewLine}";
         }
     }
--- ElementsAndMaterials/SQLRequests/select_1_element_witch_no_in_materials.sql
+++ ElementsAndMaterials/SQLRequests/select_1_element_witch_no_in_materials.sql
@@ -1,36 +1,9 @@
-DECLARE @TempTable TABLE
-(
- id_good INT,
- name_good NVARCHAR(100),
- marking_good NVARCHAR(100),
- id_glasselement INT,
- marking_glasselement NVARCHAR(100),
- name_glasselement NVARCHAR(100)
-);
-
-insert into @TempTable
 select distinct
-	g.idgood,
-	g.name,
-	g.marking,
-	ge.idglasselement,
-	ge.marking,
-	ge.name
+	ge.name name_glasselement,
+	ge.idglasselement id_glasselement,
+	ge.marking marking_glasselement
 from good g
-right join glasselement ge
---on g.marking = ge.marking 
+right join glasselement ge 
 on g.name = ge.name 
 where g.name is null
-and ge.name = '{1}'
---where ge.marking like '%раскладка%'
---and g.idgoodgroup = 733
-
---and ge.typ = 0   -- стекло
---and ge.typ = 1   -- рамка
---and ge.typ = 2   -- шпроссы
---and ge.typ = 3   -- пленки
---and ge.typ = 4   -- ленты
-
---and ge.typ = {0}
-
-select marking_glasselement, name_glasselement, id_glasselement from @TempTable
+and ge.name = '{0}'
